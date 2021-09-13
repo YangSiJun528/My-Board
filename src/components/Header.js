1eff915a2c10fc6343badee6a523275eb2737c2f
YangSiJun528 <yangsijun5528@gmail.com>
2021-09-13 12:39:52 +0900

    add router of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import { Navbar, NavDropdown, Form, Nav, FormControl, Button, Container } from 'react-bootstrap'
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
 import '../css/header.css';
 
 function Header(props) {
+  let history = useHistory();
   return (
     <Navbar expand="lg" variant="light" bg="light">
       <Container>
-        <Navbar.Brand href="#">MyBoard</Navbar.Brand>
+        <Navbar.Brand onClick={ () => {history.push(`/`)}}>MyBoard</Navbar.Brand>
         {
         props.login === true
         ?        
           <Navbar.Text className="justify-content-end">
-            <a href={`/user/${props.user.name}/info}`}>{props.user.name}</a>
+            <a onClick={ () => {history.push(`/user/${props.user.name}/info`)}}>{props.user.name}</a>
           </Navbar.Text>
         :           
           <Button className="justify-content-end" variant="outline-primary">Login</Button>
